38d2e8e618d9efc3dbe862dd430224159d8343e0
Adeyemi Sadiq <adexsadiq01@gmail.com>
2024-10-02 03:28:19 +0100

    new changes

--- app/hooks/useFetchWrapper/index.tsx
+++ app/hooks/useFetchWrapper/index.tsx
@@ -1,57 +1,57 @@
-import { useCallback } from 'react';
-import { RequestMethod } from '../../enums/requestMethod.enum';
-import { RequestOptions } from '../../interfaces/requestOption.interface';
+// import { useCallback } from 'react';
+// import { RequestMethod } from '../../enums/requestMethod.enum';
+// import { RequestOptions } from '../../interfaces/requestOption.interface';
 
-export const useFetchWrapper = () => {
-  // const apiUrl = 'https://api.daowakanda.com';
-  // const apiUrl = 'https://api.daowakanda.com/api/v1/proposals';
-  // const apiUrl = 'https://daowkd-a061a68c3343.herokuapp.com/api/v1';
-  const apiUrl = 'https://daowkd-a061a68c3343.herokuapp.com';
+// export const useFetchWrapper = () => {
+//   // const apiUrl = 'https://api.daowakanda.com';
+//   // const apiUrl = 'https://api.daowakanda.com/api/v1/proposals';
+//   // const apiUrl = 'https://daowkd-a061a68c3343.herokuapp.com/api/v1';
+//   const apiUrl = 'https://daowkd-a061a68c3343.herokuapp.com';
 
-  const handleResponse = useCallback((response: any) => {
-    return response.text().then(async (text: string) => {
-      const data = text && JSON.parse(text);
+//   const handleResponse = useCallback((response: any) => {
+//     return response.text().then(async (text: string) => {
+//       const data = text && JSON.parse(text);
 
-      if (!response.ok) {
-        const error =
-          (data && data.message) ||
-          response.statusText ||
-          (data && data.errors);
+//       if (!response.ok) {
+//         const error =
+//           (data && data.message) ||
+//           response.statusText ||
+//           (data && data.errors);
 
-        return { error, code: response.status };
-      }
+//         return { error, code: response.status };
+//       }
 
-      return data;
-    });
-  }, []);
+//       return data;
+//     });
+//   }, []);
 
-  const request = useCallback((method: RequestMethod) => {
-    return (url: string, body?: any) => {
-      const requestOptions: RequestOptions = {
-        method,
-        headers: {},
-      };
+//   const request = useCallback((method: RequestMethod) => {
+//     return (url: string, body?: any) => {
+//       const requestOptions: RequestOptions = {
+//         method,
+//         headers: {},
+//       };
 
-      if (body) {
-        requestOptions.headers['Content-Type'] = 'application/json';
-        requestOptions.body = JSON.stringify(body);
-      }
+//       if (body) {
+//         requestOptions.headers['Content-Type'] = 'application/json';
+//         requestOptions.body = JSON.stringify(body);
+//       }
 
-      console.log(requestOptions);
+//       console.log(requestOptions);
 
-      return fetch(`${apiUrl}/${url}`, requestOptions as RequestInit)
-        .then((response) => handleResponse(response))
-        .catch((err) => {
-          return { error: err };
-        });
-    };
-  }, []);
+//       return fetch(`${apiUrl}/${url}`, requestOptions as RequestInit)
+//         .then((response) => handleResponse(response))
+//         .catch((err) => {
+//           return { error: err };
+//         });
+//     };
+//   }, []);
 
-  return {
-    get: request(RequestMethod.GET),
-    post: request(RequestMethod.POST),
-    put: request(RequestMethod.PUT),
-    delete: request(RequestMethod.DELETE),
-    patch: request(RequestMethod.PATCH),
-  };
-};
+//   return {
+//     get: request(RequestMethod.GET),
+//     post: request(RequestMethod.POST),
+//     put: request(RequestMethod.PUT),
+//     delete: request(RequestMethod.DELETE),
+//     patch: request(RequestMethod.PATCH),
+//   };
+// };
